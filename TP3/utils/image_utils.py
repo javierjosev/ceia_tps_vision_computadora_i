0a24f7efb4a2ad04fc91cd434c9b030c21d1cce4
javierjosev <javierjosev@gmail.com>
2023-11-28 15:23:27 -0300

    TP 3 - Item 3

--- TP3/utils/image_utils.py
+++ TP3/utils/image_utils.py
@@ -1,21 +1,106 @@
 import numpy as np
+from matplotlib import pyplot as plt
+import cv2 as cv
 
 
-def fm_image_quality_measure(image):
-    # Step 1: Compute the Fourier Transform (F) of the image
-    F = np.fft.fft2(image)
-    # Step 2: Shift the origin of F to the center (Fc)
-    Fc = np.fft.fftshift(F)
-    # Step 3: Calculate the absolute value of the centered Fourier transform (AF)
-    AF = np.abs(Fc)
-    # Step 4: Calculate the maximum value of the frequency component in F (M)
-    M = np.max(AF)
-    # Step 5: Calculate the threshold value (thres)
-    threshold = M / 1000
-    # Step 6: Count the number of pixels in F whose value is greater than thres (TH)
-    Th = np.sum(AF > threshold)
-    # Calculate the Image Quality measure (FM)
-    FM = Th / (image.shape[0] * image.shape[1])
-    return FM
+
+def pyplot_image_show(image):
+
+    if len(image.shape) == 2:
+        # Imagen en escala de grises
+        plt.imshow(image, cmap='gray')
+    else:
+        # Imagen a color
+        plt.imshow(cv.cvtColor(image, cv.COLOR_BGR2RGB))
+
+    plt.axis('off')  # Oculta los ejes
+    plt.show()
+    
 
 
+def absolute_central_moment(image):
+    # Step 1: Calculate the image histogram
+    hist, bins = np.histogram(image.flatten(), bins=256, range=[0, 256])
+
+    # Step 2: Calculate the mean intensity value (u) of the histogram
+    u = np.mean(hist)
+
+    # Unique k gray-level values
+    k_values = np.unique(hist)
+
+    # Step 3: Get the number of gray levels (L)
+    # L = len(k_values)
+
+    # Step 4: Initialize ACMo to zero
+    ACMo = 0
+
+    # Step 5: Calculate ACMo using the formula
+    for k in k_values:
+        Pk = np.count_nonzero(hist == k) / np.sum(hist)  # Relative frequency of the k-th gray-level
+        ACMo += abs(k - u) * Pk
+
+    return ACMo
+
+
+def non_max_suppression(boxes, probs=None, overlapThresh=0.3):
+	# if there are no boxes, return an empty list
+	if len(boxes) == 0:
+		return []
+
+	# if the bounding boxes are integers, convert them to floats -- this
+	# is important since we'll be doing a bunch of divisions
+	if boxes.dtype.kind == "i":
+		boxes = boxes.astype("float")
+
+	# initialize the list of picked indexes
+	pick = []
+
+	# grab the coordinates of the bounding boxes
+	x1 = boxes[:, 0]
+	y1 = boxes[:, 1]
+	x2 = boxes[:, 2]
+	y2 = boxes[:, 3]
+
+	# compute the area of the bounding boxes and grab the indexes to sort
+	# (in the case that no probabilities are provided, simply sort on the
+	# bottom-left y-coordinate)
+	area = (x2 - x1 + 1) * (y2 - y1 + 1)
+	idxs = y2
+
+	# if probabilities are provided, sort on them instead
+	if probs is not None:
+		idxs = probs
+
+	# sort the indexes
+	idxs = np.argsort(idxs)
+
+	# keep looping while some indexes still remain in the indexes list
+	while len(idxs) > 0:
+		# grab the last index in the indexes list and add the index value
+		# to the list of picked indexes
+		last = len(idxs) - 1
+		i = idxs[last]
+		pick.append(i)
+
+		# find the largest (x, y) coordinates for the start of the bounding
+		# box and the smallest (x, y) coordinates for the end of the bounding
+		# box
+		xx1 = np.maximum(x1[i], x1[idxs[:last]])
+		yy1 = np.maximum(y1[i], y1[idxs[:last]])
+		xx2 = np.minimum(x2[i], x2[idxs[:last]])
+		yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+		# compute the width and height of the bounding box
+		w = np.maximum(0, xx2 - xx1 + 1)
+		h = np.maximum(0, yy2 - yy1 + 1)
+
+		# compute the ratio of overlap
+		overlap = (w * h) / area[idxs[:last]]
+
+		# delete all indexes from the index list that have overlap greater
+		# than the provided overlap threshold
+		idxs = np.delete(idxs, np.concatenate(([last],
+			np.where(overlap > overlapThresh)[0])))
+
+	# return only the bounding boxes that were picked
+	return boxes[pick].astype("int")
--- TP3/utils/template_matching.py
+++ TP3/utils/template_matching.py
@@ -5,25 +5,39 @@ import matplotlib.pyplot as plt
 from utils import image_utils
 
 
-def mult_template_matching_canning_with_temp_resizing(template_bgr, image_bgr, method, canny_threshold1, canny_threshold2, min_resolution_coeff, threshold, debug=False):
 
-    # If image FM quality is less than 0.7, size is increased
-    fm_image_quality = image_utils.fm_image_quality_measure(image_bgr)
-    fm_scale = 1.5
-    if fm_image_quality < 0.78:
-        image_bgr = cv.resize(image_bgr, (int(image_bgr.shape[1] * fm_scale), int(image_bgr.shape[0] * fm_scale)), interpolation=cv.INTER_LINEAR)
-        #cv.resize(coca_multi_img, (new_width, new_height), interpolation=cv.INTER_LINEAR)
+def template_matching_canning_with_temp_resizing(template_bgr, image_bgr, method, canny_threshold1, canny_threshold2, min_resolution_coeff, debug=False):
 
-    # Converting image to grayscale
+     # Converting image to grayscale
     image = cv.cvtColor(image_bgr, cv.COLOR_BGR2GRAY)
+    # Converting template to grayscale
+    template = cv.cvtColor(template_bgr, cv.COLOR_BGR2GRAY)
+
+    # image & template dimensions
+    image_height, image_width = image.shape
+    template_height, template_width = template.shape
+
+    # Scaling and quality corrections
+    # If the relation between the image/template is more than 3 times, the image size is reduced by half
+    fm_scale_min = 0.5
+    if image_width/template_width > 3:
+        image_bgr = cv.resize(image_bgr, (int(image_bgr.shape[1] * fm_scale_min), int(image_bgr.shape[0] * fm_scale_min)), interpolation=cv.INTER_LINEAR)
+        image = cv.resize(image, (int(image.shape[1] * fm_scale_min), int(image.shape[0] * fm_scale_min)), interpolation=cv.INTER_LINEAR)
+        image_height, image_width = image.shape
+    # If the ACM quality is less than 0.7, the image size is doubled
+    fm_scale_max = 2
+    image_qual_acm = image_utils.absolute_central_moment(image)
+    if image_qual_acm < 0.7:
+        image_bgr = cv.resize(image_bgr, (int(image_bgr.shape[1] * fm_scale_max), int(image_bgr.shape[0] * fm_scale_max)), interpolation=cv.INTER_LINEAR)
+        image = cv.resize(image, (int(image.shape[1] * fm_scale_max), int(image.shape[0] * fm_scale_max)), interpolation=cv.INTER_LINEAR)
+        image_height, image_width = image.shape
+
     ## Preprocessing image ###############
     image = cv.GaussianBlur(image, (5, 5), 0)
     image = cv.normalize(image, None, 0, 255, cv.NORM_MINMAX)
     # detect edges in the grayscale image
     image_edged = cv.Canny(image, canny_threshold1, canny_threshold2)
     
-    # Converting template to grayscale
-    template = cv.cvtColor(template_bgr, cv.COLOR_BGR2GRAY)
     ## Preprocessing template ###############
     template = cv.GaussianBlur(template, (5, 5), 0)
     template = cv.normalize(template, None, 0, 255, cv.NORM_MINMAX)
@@ -33,7 +47,6 @@ def mult_template_matching_canning_with_temp_resizing(template_bgr, image_bgr, m
     # found keeps track of the region and scale of the image with the best match.
     # found initialization
     found = (0, 0, 0, 0)
-    best_result = None
 
     # image & template dimensions
     image_height, image_width = image.shape
@@ -45,92 +58,6 @@ def mult_template_matching_canning_with_temp_resizing(template_bgr, image_bgr, m
 
     # loop over the scales of the image
     for scale in np.linspace(0.1, 1.0, 20)[::-1]:
-        
-        template_resized = cv.resize(template_edged, (int(template_edged.shape[1] * scale), int(template_edged.shape[0] * scale)))
-        tH_temp, tW_temp = template_resized.shape
-
-        # Breaking out the for loop because of insufficient template resolution. As the image is scaled without losing aspect ratio
-        # we can use width or height to calculate the coefficient
-        resolution_coeff = tW_temp/template_width
-        if resolution_coeff > min_resolution_coeff:
-            tH = tH_temp
-            tW = tW_temp
-        else:
-            break
-
-        # matching to find the template in the image
-        result = cv.matchTemplate(image_edged, template_resized, eval(method))
-        
-        (_, maxVal, _, maxLoc) = cv.minMaxLoc(result)
-
-        # check to see if the iteration should be visualized
-        if debug:
-            # draw a bounding box around the detected region
-            clone = np.dstack([image_edged, image_edged, image_edged])
-            cv.rectangle(clone, (maxLoc[0], maxLoc[1]), (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
-            cv.putText(clone, f"maxVal: {maxVal}", (10,20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-            maxVal_found, _, _, _ = found
-            cv.putText(clone, f"maxVal found: {maxVal_found}", (10,40), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-            cv.putText(clone, f"Scaling factor: {scale}", (10,60), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-            template_resized_fm = image_utils.fm_image_quality_measure(template_resized)
-            cv.putText(clone, f"FM: {template_resized_fm}", (10,80), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-            cv.putText(clone, f"Resolution coeff: {resolution_coeff}", (10,100), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-            cv.imshow(f"Visualizing iteration", clone)
-            cv.imshow("Resized template", template_resized)
-            cv.waitKey(0)
-        
-        # if we have found a new maximum correlation value, the found variable is updated
-        if maxVal > found[0]:
-            # found = (maxVal, maxLoc, r)
-            found = (maxVal, maxLoc, tW, tH)
-            best_result = result
-    
-    # gets the found values and draws the result bounding box
-    clone_result = np.dstack(cv.split(image_bgr))
-    loc = np.where( best_result >= threshold)
-    for pt in zip(*loc[::-1]):
-        # cv.rectangle(clone_result, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-        # cv.rectangle(clone_result, (maxLoc[0], maxLoc[1]), (maxLoc[0] + tW, maxLoc[1] + tH), (255, 0, 255), 2)
-        cv.rectangle(clone_result, pt, (pt[0] + tW, pt[1] + tH), (0,0,255), 2)
-
-    cv.imshow(f"Visualizing Result", clone_result)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-
-
-
-def template_matching_canning_with_temp_resizing(template_bgr, image_bgr, method, canny_threshold1, canny_threshold2, min_resolution_coeff, debug=False):
-
-    # Converting image to grayscale
-    image = cv.cvtColor(image_bgr, cv.COLOR_BGR2GRAY)
-    ## Preprocessing image ###############
-    image = cv.GaussianBlur(image, (5, 5), 0)
-    image = cv.normalize(image, None, 0, 255, cv.NORM_MINMAX)
-    # detect edges in the grayscale image
-    image_edged = cv.Canny(image, canny_threshold1, canny_threshold2)
-    
-    # Converting template to grayscale
-    template = cv.cvtColor(template_bgr, cv.COLOR_BGR2GRAY)
-    ## Preprocessing template ###############
-    template = cv.GaussianBlur(template, (5, 5), 0)
-    template = cv.normalize(template, None, 0, 255, cv.NORM_MINMAX)
-    # detect edges in the grayscale template
-    template_edged = cv.Canny(template, canny_threshold1, canny_threshold2)
-
-    # found keeps track of the region and scale of the image with the best match.
-    # found initialization
-    found = (0, 0, 0, 0)
-
-    # image & template dimensions
-    image_height, image_width = image.shape
-    template_height, template_width = template_edged.shape
-
-    # scale the template to the image size without losing aspect ratio
-    scaling_factor = min(image_width / template_width, image_height / template_height)
-    template_edged = cv.resize(template_edged, None, fx=scaling_factor, fy=scaling_factor)
-
-    # loop over the scales of the image
-    for scale in np.linspace(0.2, 1.0, 20)[::-1]:
         
         template_resized = cv.resize(template_edged, (int(template_edged.shape[1] * scale), int(template_edged.shape[0] * scale)))
         tH, tW = template_resized.shape
@@ -167,9 +94,132 @@ def template_matching_canning_with_temp_resizing(template_bgr, image_bgr, method
     # gets the found values and draws the result bounding box
     (maxVal, maxLoc, tW, tH) = found
     clone_result = np.dstack(cv.split(image_bgr))
-    cv.rectangle(clone_result, (maxLoc[0], maxLoc[1]), (maxLoc[0] + tW, maxLoc[1] + tH), (255, 0, 255), 2)
-    cv.putText(clone_result, f"maxVal found: {maxVal}", (maxLoc[0], maxLoc[1]+15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2)
-    cv.imshow(f"Visualizing Result", clone_result)
+    cv.rectangle(clone_result, (maxLoc[0], maxLoc[1]), (maxLoc[0] + tW, maxLoc[1] + tH), (255, 0, 0), 3)
+    cv.putText(clone_result, f"maxVal found: {maxVal}", (maxLoc[0], maxLoc[1]+15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+    
+    image_utils.pyplot_image_show(clone_result)
+    # cv.imshow(f"Visualizing Result", clone_result)
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
 
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+
+
+def mult_template_matching_canning_with_temp_resizing(template_bgr, image_bgr, method, canny_threshold1, canny_threshold2, min_resolution_coeff, threshold, debug=False):
+
+    # Converting image to grayscale
+    image = cv.cvtColor(image_bgr, cv.COLOR_BGR2GRAY)
+    # Converting template to grayscale
+    template = cv.cvtColor(template_bgr, cv.COLOR_BGR2GRAY)
+
+    # image & template dimensions
+    image_height, image_width = image.shape
+    template_height, template_width = template.shape
+
+    # Scaling and quality corrections
+    # If the relation between the image/template is more than 3 times, the image size is reduced by half
+    fm_scale_min = 0.5
+    if image_width/template_width > 3:
+        image_bgr = cv.resize(image_bgr, (int(image_bgr.shape[1] * fm_scale_min), int(image_bgr.shape[0] * fm_scale_min)), interpolation=cv.INTER_LINEAR)
+        image = cv.resize(image, (int(image.shape[1] * fm_scale_min), int(image.shape[0] * fm_scale_min)), interpolation=cv.INTER_LINEAR)
+        image_height, image_width = image.shape
+    # If the ACM quality is less than 0.7, the image size is doubled
+    fm_scale_max = 2
+    image_qual_acm = image_utils.absolute_central_moment(image)
+    if image_qual_acm < 0.7:
+        image_bgr = cv.resize(image_bgr, (int(image_bgr.shape[1] * fm_scale_max), int(image_bgr.shape[0] * fm_scale_max)), interpolation=cv.INTER_LINEAR)
+        image = cv.resize(image, (int(image.shape[1] * fm_scale_max), int(image.shape[0] * fm_scale_max)), interpolation=cv.INTER_LINEAR)
+        image_height, image_width = image.shape
+
+    ## Preprocessing image ###############
+    image = cv.GaussianBlur(image, (5, 5), 0)
+    image = cv.normalize(image, None, 0, 255, cv.NORM_MINMAX)
+    # detect edges in the grayscale image
+    image_edged = cv.Canny(image, canny_threshold1, canny_threshold2)
+    
+    ## Preprocessing template ###############
+    template = cv.GaussianBlur(template, (5, 5), 0)
+    template = cv.normalize(template, None, 0, 255, cv.NORM_MINMAX)
+    # detect edges in the grayscale template
+    template_edged = cv.Canny(template, canny_threshold1, canny_threshold2)
+
+    template_height, template_width = template_edged.shape
+
+    # found keeps track of the region and scale of the image with the best match.
+    # found initialization
+    found = (0, 0, 0, 0)
+    best_result = None
+
+    # scale the template to the image size without losing aspect ratio
+    scaling_factor = min(image_width / template_width, image_height / template_height)
+    template_edged = cv.resize(template_edged, None, fx=scaling_factor, fy=scaling_factor)
+
+    # loop over the scales of the image
+    for scale in np.linspace(0.1, 1.0, 20)[::-1]:
+        
+        template_resized = cv.resize(template_edged, (int(template_edged.shape[1] * scale), int(template_edged.shape[0] * scale)))
+        tH_temp, tW_temp = template_resized.shape
+
+        # Breaking out the for loop because of insufficient template resolution. As the image is scaled without losing aspect ratio
+        # we can use width or height to calculate the coefficient
+        resolution_coeff = tW_temp/template_width
+        if resolution_coeff > min_resolution_coeff:
+            tH = tH_temp
+            tW = tW_temp
+        else:
+            break
+
+        # matching to find the template in the image
+        result = cv.matchTemplate(image_edged, template_resized, eval(method))
+        
+        (_, maxVal, _, maxLoc) = cv.minMaxLoc(result)
+
+        # check to see if the iteration should be visualized
+        if debug:
+            # draw a bounding box around the detected region
+            clone = np.dstack([image_edged, image_edged, image_edged])
+            cv.rectangle(clone, (maxLoc[0], maxLoc[1]), (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
+            cv.putText(clone, f"maxVal: {maxVal}", (10,20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            maxVal_found, _, _, _ = found
+            cv.putText(clone, f"maxVal found: {maxVal_found}", (10,40), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv.putText(clone, f"Scaling factor: {scale}", (10,60), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            template_resized_acm = image_utils.absolute_central_moment(template_resized)
+            cv.putText(clone, f"ACM: {template_resized_acm}", (10,80), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv.putText(clone, f"Resolution coeff: {resolution_coeff}", (10,100), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv.imshow(f"Visualizing iteration", clone)
+            cv.imshow("Resized template", template_resized)
+            cv.waitKey(0)
+        
+        # if we have found a new maximum correlation value, the found variable is updated
+        if maxVal > found[0]:
+            # found = (maxVal, maxLoc, r)
+            found = (maxVal, maxLoc, tW, tH)
+            best_result = result
+    
+    # gets the found values and draws the result bounding box
+    (maxVal, maxLoc, tW, tH) = found
+    clone_result = np.dstack(cv.split(image_bgr))
+
+    (y_points, x_points) = np.where(best_result >= threshold) 
+    # initialize our list of rectangles 
+    boxes = list() 
+    # loop over the starting (x, y)-coordinates again 
+    for (x, y) in zip(x_points, y_points): 
+        # update our list of rectangles 
+        boxes.append((x, y, x + tW, y + tH)) 
+    # apply non-maxima suppression to the rectangles this will create a single bounding box 
+    boxes = image_utils.non_max_suppression(np.array(boxes)) 
+    # loop over the final bounding boxes 
+    for (x1, y1, x2, y2) in boxes: 
+        # draw the bounding box on the image 
+        cv.rectangle(clone_result, (x1, y1), (x2, y2), (255, 0, 0), 3) 
+
+    # if no rectangle is selected because of the threshold, it draws the maximum
+    if len(boxes) == 0:
+        cv.rectangle(clone_result, (maxLoc[0], maxLoc[1]), (maxLoc[0] + tW, maxLoc[1] + tH), (255, 0, 0), 3)
+        #  cv.putText(clone_result, f"maxVal found: {maxVal}", (maxLoc[0], maxLoc[1]+15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+    image_utils.pyplot_image_show(clone_result)
+    # cv.imshow(f"Visualizing Result", clone_result)
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+
